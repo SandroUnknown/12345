152c8005aac6a05ac156db20c3e4808dca4104d8
SandroUnknown <108019459+SandroUnknown@users.noreply.github.com>
2024-12-02 14:44:40 +0300

    Update BrowserstackDriver.java

--- src/test/java/drivers/BrowserstackDriver.java
+++ src/test/java/drivers/BrowserstackDriver.java
@@ -10,34 +10,35 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 import javax.annotation.Nonnull;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.HashMap;
 
 public class BrowserstackDriver implements WebDriverProvider {
     @Nonnull
     @Override
     public WebDriver createDriver(@Nonnull Capabilities capabilities) {
-        MutableCapabilities caps = new MutableCapabilities();
+        /*MutableCapabilities caps = new MutableCapabilities();
 
         // Set your access credentials
         caps.setCapability("browserstack.user", "alex_qN6X0H");
-        caps.setCapability("browserstack.key", "r9siN8SFjHzuw37LHuDc");
+        caps.setCapability("browserstack.key", "r9siN8SFjHzuw37LHuDc");*/
 
 
-/*DesiredCapabilities capabilities = new DesiredCapabilities();
+DesiredCapabilities cap2 = new DesiredCapabilities();
 HashMap<String, Object> bstackOptions = new HashMap<String, Object>();
 bstackOptions.put("userName", "alex_qN6X0H");
 bstackOptions.put("accessKey", "r9siN8SFjHzuw37LHuDc");
-capabilities.setCapability("platformName", "android");
-capabilities.setCapability("appium:platformVersion", "15.0");
-capabilities.setCapability("appium:deviceName", "Google Pixel 9");
-capabilities.setCapability("appium:app", "bs://c700ce60cf13ae8ed97705a55b8e022f13c5827c");
-capabilities.setCapability("bstack:options", bstackOptions);*/
+cap2.setCapability("platformName", "android");
+cap2.setCapability("appium:platformVersion", "15.0");
+cap2.setCapability("appium:deviceName", "Google Pixel 9");
+cap2.setCapability("appium:app", "bs://c700ce60cf13ae8ed97705a55b8e022f13c5827c");
+cap2.setCapability("bstack:options", bstackOptions);
 
 
 
-        
+        /*
         // Set URL of the application under test
-        caps.setCapability("appium:app", "bs://c700ce60cf13ae8ed97705a55b8e022f13c5827c");
-/*
+        caps.setCapability("app", "bs://c700ce60cf13ae8ed97705a55b8e022f13c5827c");
+
         // Specify device and os_version for testing
         caps.setCapability("device", "Google Pixel 3");
         caps.setCapability("os_version", "9.0");

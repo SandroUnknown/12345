5e5f8cdd8184a2a763c7d24c9141d36a508451b5
SandroUnknown <108019459+SandroUnknown@users.noreply.github.com>
2024-12-02 15:06:04 +0300

    Update BrowserstackDriver.java

--- src/test/java/drivers/BrowserstackDriver.java
+++ src/test/java/drivers/BrowserstackDriver.java
@@ -34,11 +34,11 @@ caps.setCapability("appium:app", "bs://c700ce60cf13ae8ed97705a55b8e022f13c5827c"
 caps.setCapability("bstack:options", bstackOptions);*/
 
 // bs://9fg0rt302be241e6524ebc67253ecdee266343
-        // wiki - bs://c700ce60cf13ae8ed97705a55b8e022f13c5827c
+// wiki - bs://c700ce60cf13ae8ed97705a55b8e022f13c5827c
 
         
         // Set URL of the application under test
-        //caps.setCapability("app", "bs://c700ce60cf13ae8ed97705a55b8e022f13c5827c");
+        caps.setCapability("app", "bs://sample.app");
 
         // Specify device and os_version for testing
         caps.setCapability("device", "Google Pixel 3");

c1850c48d9521ee5645c287a8e05e265b9c34870
SandroUnknown <108019459+SandroUnknown@users.noreply.github.com>
2024-12-02 14:48:10 +0300

    Update BrowserstackDriver.java

--- src/test/java/drivers/BrowserstackDriver.java
+++ src/test/java/drivers/BrowserstackDriver.java
@@ -16,14 +16,14 @@ public class BrowserstackDriver implements WebDriverProvider {
     @Nonnull
     @Override
     public WebDriver createDriver(@Nonnull Capabilities capabilities) {
-        /*MutableCapabilities caps = new MutableCapabilities();
+        MutableCapabilities caps = new MutableCapabilities();
 
         // Set your access credentials
         caps.setCapability("browserstack.user", "alex_qN6X0H");
-        caps.setCapability("browserstack.key", "r9siN8SFjHzuw37LHuDc");*/
+        caps.setCapability("browserstack.key", "r9siN8SFjHzuw37LHuDc");
 
 
-DesiredCapabilities caps = new DesiredCapabilities();
+/*DesiredCapabilities caps = new DesiredCapabilities();
 HashMap<String, Object> bstackOptions = new HashMap<String, Object>();
 bstackOptions.put("userName", "alex_qN6X0H");
 bstackOptions.put("accessKey", "r9siN8SFjHzuw37LHuDc");
@@ -31,18 +31,19 @@ caps.setCapability("platformName", "android");
 caps.setCapability("appium:platformVersion", "15.0");
 caps.setCapability("appium:deviceName", "Google Pixel 9");
 caps.setCapability("appium:app", "bs://c700ce60cf13ae8ed97705a55b8e022f13c5827c");
-caps.setCapability("bstack:options", bstackOptions);
+caps.setCapability("bstack:options", bstackOptions);*/
 
 
 
-        /*
+        
         // Set URL of the application under test
-        caps.setCapability("app", "bs://c700ce60cf13ae8ed97705a55b8e022f13c5827c");
+       // caps.setCapability("app", "bs://c700ce60cf13ae8ed97705a55b8e022f13c5827c");
 
         // Specify device and os_version for testing
         caps.setCapability("device", "Google Pixel 3");
         caps.setCapability("os_version", "9.0");
 
+        /*
         // Set other BrowserStack capabilities
         caps.setCapability("project", "First Java Project");
         caps.setCapability("build", "browserstack-build-1");
